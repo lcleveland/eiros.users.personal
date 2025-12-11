546604c567d2eece0712a15eaa265a004d6819df
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-11 15:50:29 -0600

    Update user configurations: Reorganize flake.nix structure & rename Colemak DH setting

--- flake.nix
+++ flake.nix
@@ -1,6 +1,14 @@
-{ ... }:
 {
-  eiros.users.lcleveland.mangowc.settings = {
-    xkb_rules_variant = "colemak_dh";
-  };
+  description = "Reusable NixOS user configurations";
+  outputs =
+    { self }@inputs:
+    let
+      import_modules = import ./resources/nix/import_modules.nix;
+      users = (import_modules ./users);
+    in
+    {
+      inputs = inputs;
+      nixosModules.default = users;
+    };
+  inputs = { };
 }
