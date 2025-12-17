8758ff5bb5ab5339955bb709f5df1f2503a0af80
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-17 08:48:25 -0600

    fix monitors

--- users/lcleveland/monitors.nix
+++ users/lcleveland/monitors.nix
@@ -1,8 +1,8 @@
 {config, lib, ...}:
 {
   config.eiros.users.lcleveland.mangowc.settings.monitorrule = [
-    "eDP-1,0.55,1,scroller,0,1,1920,0,1920,1080,165"
-    "DP-12,0.55,1,scroller,0,1,0,0,1920,1080,60"
-    "DP-13,0.55,1,scroller,0,1,0,1080,1920,1080,60"
+    "eDP-1,0.55,1,scroller,0,1,1920,1080,1920,1080,165"
+    "DP-12,0.55,1,scroller,0,1,0,1080,1920,1080,60"
+    "DP-13,0.55,1,scroller,2,1,0,0,1920,1080,60"
   ];
 }
