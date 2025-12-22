7a55915d1dead29a0437d9f65ee4d10d28c402f7
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-22 17:53:43 -0600

    Update keyboard_layout.nix

--- users/lcleveland/keyboard_layout.nix
+++ users/lcleveland/keyboard_layout.nix
@@ -1,13 +1,4 @@
 { config, lib, ... }:
 {
-  config = {
-    eiros.users.lcleveland.mangowc.settings = {
-      "xkb_rules_layout" = "us";
-      "xkb_rules_variant" = "colemak_dh";
-    };
-    eiros.system.desktop_environment.dank_material_shell.greeter.hyprland.sections.input = {
-      kb_layout = "us";
-      kb_variant = "colemak_dh";
-    };
-  };
+
 }
