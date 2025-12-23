378da1183994946883b29ce1a7f5fae3a17b5775
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-22 18:04:54 -0600

    Update monitors.nix

--- users/lcleveland/monitors.nix
+++ users/lcleveland/monitors.nix
@@ -1,8 +1,7 @@
 {config, lib, ...}:
 {
   config.eiros.users.lcleveland.mangowc.settings.monitorrule = [
-    "eDP-1,0.55,1,scroller,0,1,1920,1080,2560,1600,165"
-    "DP-12,0.55,1,scroller,0,1,0,1080,1920,1080,60"
-    "DP-13,0.55,1,scroller,2,1,0,0,1920,1080,60"
+    "DP-3,0.55,1,scroller,0,1,0,0,1920,1200,60"
+    "HDMI-A-2,0.55,1,scroller,0,1,0,1200,3840,1080,120"
   ];
 }
