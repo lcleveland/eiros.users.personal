515c72ccf3f421faa4826ce8bdc6e39e828dad10
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-22 19:18:56 -0600

    move monitors to hardware

--- users/lcleveland/monitors.nix
+++
@@ -1,7 +0,0 @@
-{config, lib, ...}:
-{
-  config.eiros.users.lcleveland.mangowc.settings.monitorrule = [
-    "DP-3,0.55,1,scroller,0,1,0,0,1920,1200,60"
-    "HDMI-A-2,0.55,1,scroller,0,1,0,1200,3840,1080,120"
-  ];
-}
