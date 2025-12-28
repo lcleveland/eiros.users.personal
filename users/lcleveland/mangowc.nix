14e2296be24bfa3361ae00997464f08bdc914c03
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-28 15:23:14 -0600

    fix: correct configuration path for mangowc settings

--- users/lcleveland/mangowc.nix
+++ users/lcleveland/mangowc.nix
@@ -1,11 +1,9 @@
 { config, lib, ... }:
 {
-  config = {
-    eiros.users.lcleveland.settings = {
-      enable_hotarea = 0;
-      ov_tab_mode = 1;
-      idleinhibit_ignore_visible = 1;
-      edge_scroller_pointer_focus = 0;
-    };
+  config.eiros.users.lcleveland.mangowc.settings = {
+    enable_hotarea = 0;
+    ov_tab_mode = 1;
+    idleinhibit_ignore_visible = 1;
+    edge_scroller_pointer_focus = 0;
   };
 }
