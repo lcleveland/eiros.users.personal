ee11ac89c14ae239394d0f8c4eac7ebe7f740350
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-30 10:08:34 -0600

    add binary cache

--- users/lcleveland/applications/comfyui.nix
+++ users/lcleveland/applications/comfyui.nix
@@ -1,5 +1,17 @@
 { inputs, pkgs, ... }:
 {
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://comfyui.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   services.comfyui = {
     enable = true;
     cuda = true;
