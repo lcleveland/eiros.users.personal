3ce8feaa72c3e111219633b01a404efec96f0822
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-01 19:50:54 -0600

    fix issue

--- users/lcleveland/open_webui.nix
+++ users/lcleveland/open_webui.nix
@@ -6,6 +6,22 @@
 }:
 {
   config = {
+    # Inline overlay: relax extract-msg dependency
+    nixpkgs.overlays = [
+      (final: prev: {
+        python313Packages = prev.python313Packages.overrideScope (
+          pyFinal: pyPrev: {
+            extract-msg = pyPrev.extract-msg.overridePythonAttrs (old: {
+              nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pyFinal.pythonRelaxDepsHook ];
+
+              # relax beautifulsoup4<4.14 constraint
+              pythonRelaxDeps = (old.pythonRelaxDeps or [ ]) ++ [ "beautifulsoup4" ];
+            });
+          }
+        );
+      })
+    ];
+
     services.open-webui = {
       enable = true;
     };
