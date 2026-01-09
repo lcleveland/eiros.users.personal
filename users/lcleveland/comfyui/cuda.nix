acde6767017cc598cd4e2857d265ef118bae7d72
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-08 20:54:59 -0600

    remove cuz it don't work

--- users/lcleveland/comfyui/cuda.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, ... }:
-{
-  nix.settings = {
-    substituters = lib.mkAfter [ "https://cache.nixos-cuda.org" ];
-    trusted-public-keys = lib.mkAfter [
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-    ];
-  };
-}
