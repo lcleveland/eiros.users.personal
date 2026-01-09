0229410ce3d4ed2f8bd245d0ddfefc77f7e43ac2
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-08 20:07:26 -0600

    fix

--- users/lcleveland/trusted_user.nix
+++ users/lcleveland/trusted_user.nix
@@ -5,4 +5,3 @@
     "lcleveland"
   ];
 }
-  "scm-history-item:/home/lcleveland/Documents/eiros.users.personal?%7B%22repositoryId%22%3A%22scm0%22%2C%22historyItemId%22%3A%22ffcf92f96ea0b18ee3af6f08447133fcdc560291%22%2C%22historyItemParentId%22%3A%22a5328664b1bc6c2450714987f64b8784acdca996%22%2C%22historyItemDisplayId%22%3A%22ffcf92f%22%7D"
