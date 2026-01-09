e09d6f9879ec3d0c6f16b1bfa37cd46f42ec3476
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-08 20:27:32 -0600

    add cache

---
+++ users/lcleveland/comfyui/cuda.nix
@@ -0,0 +1,7 @@
+{ config, lib, ... }:
+{
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+}
