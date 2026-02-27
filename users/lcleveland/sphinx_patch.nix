1a4ec1a95b8154a735dafa4ffe3ee418432708bd
Lyle Cleveland <lyle.cleveland@proton.me>
2026-02-27 10:14:03 -0600

    fix hash

--- users/lcleveland/sphinx_patch.nix
+++ users/lcleveland/sphinx_patch.nix
@@ -3,7 +3,7 @@ let
   sphinx91Patch = pkgs.fetchpatch {
     url = "https://github.com/prometheusresearch/sphinxcontrib-newsfeed/commit/0d82eee.patch";
     # fill with `nix store prefetch-file --hash-type sha256 <url>`
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-3dtXp7gWOuyX27GczaJlPWXHX8EO2uyS9AkoxJT5Rn4=";
   };
 in
 {
