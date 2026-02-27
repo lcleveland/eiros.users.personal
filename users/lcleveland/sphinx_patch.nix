7414151bbfc12abfa5f6320abc8fe02aca4b36a3
Lyle Cleveland <lyle.cleveland@proton.me>
2026-02-27 10:11:35 -0600

    patch

--- users/lcleveland/sphinx_patch.nix
+++ users/lcleveland/sphinx_patch.nix
@@ -1,18 +1,15 @@
-{ lib, pkgs, ... }:
-
+{ pkgs, ... }:
 let
-  # Upstream fix is in prometheusresearch/sphinxcontrib-newsfeed PR #7 (merged),
-  # commit 0d82eee. :contentReference[oaicite:1]{index=1}
   sphinx91Patch = pkgs.fetchpatch {
     url = "https://github.com/prometheusresearch/sphinxcontrib-newsfeed/commit/0d82eee.patch";
-    # Fill this in with: nix store prefetch-file --hash-type sha256 <url>
+    # fill with `nix store prefetch-file --hash-type sha256 <url>`
     hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
 in
 {
   nixpkgs.overlays = [
     (final: prev: {
-      python3Packages = prev.python3Packages.overrideScope' (
+      python3Packages = prev.python3Packages.overrideScope (
         pyFinal: pyPrev: {
           sphinxcontrib-newsfeed = pyPrev.sphinxcontrib-newsfeed.overridePythonAttrs (old: {
             patches = (old.patches or [ ]) ++ [ sphinx91Patch ];
